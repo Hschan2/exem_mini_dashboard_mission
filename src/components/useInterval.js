d9e79d923c581280c832f69bcbfdd44b00927e23
Hchan2 <seongchan_@naver.com>
2023-03-01 17:07:44 +0900

    사용 이유 작성

--- src/components/useInterval.js
+++ src/components/useInterval.js
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from 'react';
 
 const useInterval = (callback, delay) => {
+    // 최근 들어온 콜백을 저장할 Ref
+    // useState를 사용하지 않은 이유는 리렌더링이 발생하기 때문 (setState에서 값 변경이 이뤄졌을 시, 리렌더링)
     const savedCallback = useRef(null);
 
     useEffect(() => {
+        // 콜백이 바뀔 때마다 Ref 업데이트
         savedCallback.current = callback;
     }, [callback]);
 
@@ -12,8 +15,10 @@ const useInterval = (callback, delay) => {
         savedCallback.current();
         };
 
+        // Delay에 맞춰 Interval을 새로 실행
         const timerId = setInterval(executeCallback, delay);
 
+        // Unmount될 때, ClearInterval 실행
         return () => clearInterval(timerId);
     }, []);
 };
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,4 +7,5 @@ const chartHandlers = [
   rest.get('/pie', pieResolver),
   rest.get('/value', valueResolver),
 ];
+// handlers = [chartHandlers]에서 []를 지운 이유는 이미 chartHandlers를 배열로 한 번 묶었기 때문
 export const handlers = chartHandlers;
